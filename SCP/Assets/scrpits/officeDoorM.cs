58344a415af455ddad06d2752f0d87570b1f3adf
DESKTOP-EIQ36Q5\andre <yerilth2@gmail.com>
2019-05-28 21:47:48 +0930

    seting up some lighting
    up date

--- SCP/Assets/scrpits/officeDoorM.cs
+++ SCP/Assets/scrpits/officeDoorM.cs
@@ -8,11 +8,19 @@ public class officeDoorM : MonoBehaviour,IIntractable
     public bool DoorON,wait;
     public GameObject Door;
     public Transform[] doorPos;
+    public Transform[] buttonPos;
+    public GameObject [] buttons;
     public AudioSource ButtonSource,DoorSource;
     public AudioClip DoorClickSound,DoorSound;
+ 
+    public void Start()
+    {
+        buttons[0].transform.position = buttonPos[DoorON ? 1 : 0].position;
+        buttons[1].transform.position = buttonPos[DoorON ? 3 : 2].position;
 
-  
-  public  void PickUp(Transform destnation)
+    }
+
+    public  void PickUp(Transform destnation)
     {
          if(wait  == false && GameM.LockDown == false)
         {
@@ -29,6 +37,9 @@ public class officeDoorM : MonoBehaviour,IIntractable
         wait = true;
         DoorON = Dooropen;
         iTween.MoveTo(Door, iTween.Hash("position", doorPos[DoorON ? 0 : 1], "time", 3f, "easetype", iTween.EaseType.easeInOutExpo));
+        iTween.MoveTo(buttons[0], iTween.Hash("position", buttonPos[DoorON ? 1 : 0], "time", 0.5f, "easetype", iTween.EaseType.easeInOutBounce));
+        iTween.MoveTo(buttons[1], iTween.Hash("position", buttonPos[DoorON ? 3 : 2], "time", 0.5f, "easetype", iTween.EaseType.easeInOutBounce));
+
     }
     public void Drop()
     {
